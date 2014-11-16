e8dd930dadb324922b4cda435d1bf42bc71b7777
George Plukov <gplukov@gmail.com>
2014-11-16 17:22:18 -0500

    Updated equals to string comparison, added test main method

--- src/cs2s03/ListChar.java
+++ src/cs2s03/ListChar.java
@@ -1,10 +1,14 @@
 package cs2s03;
 
-public class ListChar implements MyStack{
-	
+public class ListChar implements MyStack {
+
 	private char head;
 	private ListChar tail;
-	
+
+	ListChar() {
+		// creates the null list
+	}
+
 	ListChar(char head, ListChar tail) {
 		this.head = head;
 		this.tail = tail;
@@ -18,33 +22,58 @@ public class ListChar implements MyStack{
 	// pop an item off the top of the stack
 	public void pop() {
 		// make current head into the head of the tail
-		this.head = tail.head;
+		this.head = this.tail.head;
 		// make current tail into the tail of the current tail
-		this.tail = tail.tail;
+		this.tail = this.tail.tail;
 	}
+
 	// push an item onto the stack
 	public void push(char c) {
-		// make the current tail equal to a new list that is essentially the current list
-		this.tail = new ListChar(this.head,this.tail);
+		// make the current tail equal to a new list that is essentially the
+		// current list
+		this.tail = new ListChar(this.head, this.tail);
 		// add the new head to this list
 		this.head = c;
 	}
-	
+
 	@Override
 	public boolean isEmpty() {
 		// check to see if the head and tail are null
-		if (head == '\0' && tail == null){
-			return true; 
+		if (head == '\0' && tail == null) {
+			return true;
 		}
 		return false;
 	}
-	public String toString(){
-		if ( tail == null){
-			return head + "";
+	@Override
+	public boolean equals(Object compareObj) {
+		if (compareObj == this) // comparing object to itself
+			return true;
+		if (!(compareObj instanceof ListChar)) // check to see if you are actually comparing two of the same type of objects
+			return false;
+		
+		// Turn the the two objects into strings which will make thaem easier to compare		
+		String list = this.head + " " + this.tail.toString();
+		String objList = compareObj.toString();
+		
+		if (list.equals(objList))
+			return true;
+		else
+			return false;
+	}
+
+	public String toString() {
+		if (tail == null) {
+			return head + " ";
 		}
 		return head + " " + tail;
 	}
-	public static void main(String [] args){
-		System.out.println(new ListChar('a', new ListChar('b',new ListChar ('3',null))));
+
+	public static void main(String[] args) {
+		ListChar l1 = new ListChar('a', new ListChar('b', new ListChar('3',
+				null)));
+		ListChar l2 = new ListChar('a', new ListChar('b',new ListChar('3',
+				null)));
+
+		System.out.println(l1.equals(l2));
 	}
 }
